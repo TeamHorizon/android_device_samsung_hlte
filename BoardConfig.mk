93171102ab219118635a60002b81791968eaf1c0
erwan Leboucher <erwanleboucher@gmail.com>
2015-03-16 21:26:54 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,13 +34,18 @@ TARGET_USERIMAGES_USE_EXT4 := true
 -include vendor/samsung/hlte/BoardConfigVendor.mk
 
 
-# BlissPop Cinfigs
-Bliss_Build_Block := 1
 
-# Bissiful building options
-#BLISSIFY :=true
-BLISS_O3 := true
-BLISS_STRICT := true
-BLISS_KRAIT := true
+# BlissPop Configs
 TARGET_TC_ROM := 4.8-sm
 TARGET_TC_KERNEL := 4.8-sm
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_STRICT := true
+BLISS_GRAPHITE := true
+BLISS_KRAIT := true
+BLISS_PIPE := true
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
